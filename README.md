c36d7fff5cad3eb1edc2994521a89157ce62795e
Lucas <lucas.hilaire@efrei.net>
2024-05-19 15:55:34 +0200

    setup docker env + data simulator with kafka

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# safety-watcher
+# Safety watcher
+## Context :
+The goal of this project is to provide a scalable solution for a city to monitor and deal with suspicious behavior in daily life situations to maintain public safety. This system needs to be efficient and able to process a large amount of data.
+For that we’ll:
+-	 Collect real-time data from city CCTV and sensors
+-	Analyze the data to identify suspicious activities or behaviors.
+-	Trigger alerts for law enforcement agencies to intervene when necessary.
+-	Store the data to perform analytics and identify patterns/trends in criminal activities or public disturbances. Analysts will use those insights to allocate resources effectively and develop proactive strategies to prevent crime.
+
+
---
+++ safety-watcher_simulator/kafka_producer.py
@@ -0,0 +1,103 @@
+import uuid
+import csv
+import random
+import requests
+from math import sqrt
+import json
+from kafka import KafkaProducer
+import time
+import logging
+
+def extract_data():
+    res = requests.get("https://randomuser.me/api").json()
+    res = res['results'][0]
+    return res
+
+def get_behaviors(num_of_words):
+    with open("data/behaviors.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        n = random.sample(range(1,28),num_of_words)
+        line = 1
+        random_words = []
+        for row in csv_reader:
+            if line in n:
+                random_words.append(row)
+            line+=1
+        return random_words
+
+'''
+To make data more realistic we consider the center of map (50,50) and we consider 
+more the behavior detected in the center more it is likely to be suspicious
+'''
+def get_location_coef(location):
+    center = (50,50)
+    distance = sqrt(pow((location[0] - center[0]),2) + pow((location[1] - center[1]),2))
+    return round(2 - distance/50, 2)
+
+'''
+To make data more realistic we consider it is more likely to have a suspicious behavior
+depending on the age
+'''
+def get_age_coef(age):
+    if age > 15 and age < 20:
+        return 1.2
+    if age >= 20 and age < 30:
+        return 1.3
+    if age > 65:
+        return 0.7
+    return 1
+
+'''
+To make data more realistic we consider it is more likely to have a suspicious behavior
+depending on the time. Ex: if during the night or peak-time so coef is higher
+'''
+def get_hour_coef(hour):
+    if hour >= 6 and hour <= 9 or hour >=16 and hour <= 19:
+        return 1.2
+    if hour > 0 and hour < 6:
+        return 1.3
+    return 1
+
+
+def format_data(res):
+    behaviors = get_behaviors(3)
+    behaviors_score = sum(map(lambda x: int(x[1]), behaviors))
+    behaviors = list(map(lambda x: x[0], behaviors))
+    location = (round(random.uniform(1,100),2), round(random.uniform(1,100),2))
+    hour = random.randint(0,23)
+    minutes = random.randint(0,59)
+
+    data = {}
+    data['id'] = str(uuid.uuid4())
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['age'] = res['dob']['age']
+    data['behaviors'] = behaviors
+    data['location'] = location
+    data['time'] = f"{hour}:{minutes}"
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    location_coef = get_location_coef(location)
+    age_coef = get_age_coef(data['age'])
+    hour_coef = get_hour_coef(hour)
+
+    dangerosity_score = (behaviors_score * location_coef * hour_coef * age_coef) * 100 / 25 
+    data['dangerosity_score'] = int(dangerosity_score)
+    print(data)
+    return data
+
+def stream_data():
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    for i in range(100):
+        try:
+            res = extract_data()
+            res = format_data(res)
+            producer.send('users', json.dumps(res).encode('utf8'))
+        except Exception as e:
+            logging.error(e)
+            continue
+
+stream_data()
+
